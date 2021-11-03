3f5edfede74d635802b4c2ecac70af17d3235750
Jang <zn2309@naver.com>
2021-11-03 12:09:30 +0900

    chap3

--- main.c
+++ main.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
 
+void p1();
+void p2();
+void q1();
+void q1_1();
+
 int main() {
-    printf("È«±æµ¿\n È« ±æ µ¿\n È«  ±æ  µ¿\n");
+    p1();
+    q1();
+    q1_1();
     return 0;
+}
+
+void p1(){
+    printf("È«±æµ¿\n È« ±æ µ¿\n È«  ±æ  µ¿\n");
+    printf("------------------\n");
+}
+
+void q1(){
+    int n1;
+    int n2;
+    n1=1;
+    n2=2;
+    printf("%d %d\n", n1, n2);
+    printf("------------------\n");
+}
+
+void q1_1(){
+    int n1;
+    n1=1;
+    int n2;
+    n2=2;
+    printf("%d %d\n", n1, n2);
+    printf("------------------\n");
 }
