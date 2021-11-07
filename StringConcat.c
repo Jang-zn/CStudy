5e40f249194ae7b8bfdd97eb7d4ca3089180d0df
Jang <zn2309@naver.com>
2021-11-08 00:25:26 +0900

    string.h - strcat, strncat

---
+++ StringConcat.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char str1[30] = "First blood";
+    char str2[30] = "Second blood";
+
+    char str3[30] = "Simple number : ";
+    char str4[30] = "1234567890";
+
+    //case 1
+    strcat(str1,str2);
+    puts(str1);
+
+    //case 2
+    strncat(str3, str4, 7);
+    puts(str3);
+
+    return 0;
+
+
+
+}
