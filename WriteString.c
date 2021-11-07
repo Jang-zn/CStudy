02faa2fdadea124ac550095ac89a76a53c967816
Jang <zn2309@naver.com>
2021-11-07 21:38:34 +0900

    String IO

---
+++ WriteString.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main(){
+    char * str = "Simple String";
+
+    printf("1.puts test===========\n");
+    puts(str);
+    puts("Success");
+    printf("2.fputs test===========\n");
+    fputs(str,stdout);
+    printf("\n");
+    fputs("success",stdout);
+    printf("\n");
+    return 0;
+
+}
