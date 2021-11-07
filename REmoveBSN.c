8b9f1fe368407dc19b8a1d234230c31f4e1a501f
Jang <zn2309@naver.com>
2021-11-07 23:54:47 +0900

    string.h - strlen

---
+++ REmoveBSN.c
@@ -0,0 +1,19 @@
+﻿#include<stdio.h>
+#include<string.h>
+
+void removeBsn(char str[]){
+    int length = strlen(str);
+    str[length-1]=0;
+}
+
+int main(){
+    char str[100];
+    printf("문자열 입력 : ");
+    fgets(str, sizeof(str), stdin);
+    printf("길이 : %d, 내용 : %s\n", strlen(str), str);
+
+    removeBsn(str);
+    printf("길이 : %d, 내용 : %s\n", strlen(str), str);
+    printf("-----");
+    return 0;
+}
